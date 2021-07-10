d7d28dc446a7dc43700829b283cffaf319ae7252
Samuel <Samuelpmontague@gmail.com>
2021-07-10 14:42:57 -0500

    started working on test.js files

---
+++ test/Employee.test.js
@@ -0,0 +1,61 @@
+const Employee = require("../lib/Employee")
+
+describe("Employee", () => {
+    it("Should create Employee instance", () => {
+        const employee = new Employee();
+        expect(typeof(employee)).toEqual("object");
+    });
+
+    it("Should set name of employee", () => {
+        const name = "Samuel";
+        const employee = new Employee(name);
+        expect(employee.name).toEqual(name);
+    })
+
+    // using "Foo" as meta variable
+
+    it("Should set id of employee", () => {
+        const value = 22;
+        const employee = new Employee("Foo", value);
+        expect(employee.id).toEqual(value);
+    })
+
+    it("Should set email of employee", () => {
+        const value = "myemail@gmail.com";
+        const employee = new Employee("Foo", 1, value);
+        expect(employee.email).toEqual(value);
+    })
+
+    describe("getName", () => {
+        it("Should get name from getName()", () => {
+            const value = "Sam";
+            const employee = new Employee(value)
+            expect(employee.getName()).toEqual(value);
+        });
+    })
+    
+    describe("getId", () => {
+        it("Should get id from getId()", () => {
+            const value = 22;
+            const employee = new Employee("Foo", value);
+            expect(employee.getId()).toEqual(value);
+        });
+    })
+
+    describe("getEmail", () => {
+        it("Should get email from getEmail()", () => {
+        const value = "myemail@gmail.com";
+        const employee = new Employee("Foo", 1, value);
+        expect(employee.getEmail()).toEqual(value);
+        })
+    })
+
+    describe("getRole", () => {
+        it("Should get \"Employee\", from getRole()", () => {
+        const value = "Employee"
+        const employee = new Employee("Sam", 1, "myemail@gmail.com");
+        expect(employee.getRole()).toEqual(value);
+        })
+    })
+    
+})
